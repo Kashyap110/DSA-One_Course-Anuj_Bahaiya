add868da89c3b8d33b4643b7ee92d59e9fa0fab2
Kashyap Chavhan <90785451+Kashyap110@users.noreply.github.com>
2022-09-04 01:55:08 +0530

    Update kadane's algo
     for all negative values

--- MaxiumSumSubArray.java
+++ MaxiumSumSubArray.java
@@ -3,21 +3,30 @@ package array;
 public class MaxiumSumSubArray {
 	//Kaden's Algorithm
 	public static int MaxSubArraySum(int nums[]) {
-		int maxsum = 0 ;
-		int cursum = 0 ;
-		for(int i = 0; i< nums.length; i++) {
-			cursum += nums[i];
-			if(cursum>maxsum) {
-				maxsum = cursum;
-			}
-			if(cursum<0) {
-				cursum = 0;
-			}
+		int sum = 0 ;
+		int maxsum = Integer.MIN_VALUE;
+		for(int i = 0 ; i< nums.length ; i++) {
+			sum += nums[i];
+			maxsum = Math.max(sum, maxsum);
+			if (sum<0) sum = 0 ;
 		}
 		return maxsum;
+		//work in O(n) but not work for all negative
+//		int maxsum = 0 ;
+//		int cursum = 0 ;
+//		for(int i = 0; i< nums.length; i++) {
+//			cursum += nums[i];
+//			if(cursum>maxsum) {
+//				maxsum = cursum;
+//			}
+//			if(cursum<0) {
+//				cursum = 0;
+//			}
+//		}
+//		return maxsum;
 	}
 	public static void main(String[] args) {
-		int n [] = {-5,4,6,-3,4,-1};
+		int n [] = {-5,-3,-4,-1};
 		System.out.println(MaxSubArraySum(n));
 	}
 
